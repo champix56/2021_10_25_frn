f1543d4c4719900ddd5b553576673d63b2d6f1ae
alex <desorbaix@free.fr>
2021-10-25 16:39:59 +0200

    mise en place de style

--- src/App/components/Button.tsx
+++ src/App/components/Button.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 interface Props {
   text: String;
 }
 const Button: React.FC<Props> = props => {
   console.log(props);
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{props.text}</Text>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'plum',
+  },
+});
 export default Button;
