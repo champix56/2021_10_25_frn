added097957500d8a020cfc8b0c980d6da95ea44
alex <desorbaix@free.fr>
2021-10-26 23:23:56 +0200

    config

--- src/App/config/config.tsx
+++ src/App/config/config.tsx
@@ -1,4 +1,4 @@
-export const ADR_REST = 'http://localhost:5679';
+export const ADR_REST = 'http://10.0.2.2:5679';
 export enum RESSOURCES_NAME {
   products = '/products',
   categories = '/categories',
