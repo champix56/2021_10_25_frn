334df259f0e45aecacc86fe25b278cafdbfb8162
alex <desorbaix@free.fr>
2021-10-27 12:01:49 +0200

    connexion react-redux d'un cmp

--- src/App/components/ProductSearch/ProductSearch.tsx
+++ src/App/components/ProductSearch/ProductSearch.tsx
@@ -1,20 +1,24 @@
-import {resolvePreset} from '@babel/core';
 import React from 'react';
 import {View, TextInput, StyleSheet} from 'react-native';
-interface Props {
-  value: string;
-  onChange: Function;
-}
+import {connect} from 'react-redux';
+import {PRODUCTS_ACTIONS} from '../../store/store';
+interface IProps {}
+//declaration auto des interface des maps
+type StateProps = ReturnType<typeof mapStateProps>;
+type DispatchProps = typeof mapsDispatchToProps;
+//creation de l'interface composée des props + types des maps
+type Props = StateProps & DispatchProps & IProps;
 const ProductSearch = (props: Props) => {
+  console.log(props);
   return (
     <View style={style.container}>
       <TextInput
         placeholder="Recherche"
+        value={props.searchValue}
         style={style.searchInput}
-        value={props.value}
         onChangeText={textValue => {
           console.log(textValue);
-          props.onChange(textValue);
+          props.changeSearchValue(textValue);
         }}
       />
       {/* <View style={style.searchButton}>
@@ -37,4 +41,14 @@ const style = StyleSheet.create({
     marginBottom: 5,
   },
 });
-export default ProductSearch;
+//connexion au store avec mapping des values //functions dans les props
+function mapStateProps(state: any, own: any) {
+  return {searchValue: state.datas.search, ...own};
+}
+function mapsDispatchToProps(dispatch: Function) {
+  return {
+    changeSearchValue: (value: string) =>
+      dispatch({type: PRODUCTS_ACTIONS.SET_SEARCH, value: value}),
+  };
+}
+export default connect(mapStateProps, mapsDispatchToProps)(ProductSearch);
