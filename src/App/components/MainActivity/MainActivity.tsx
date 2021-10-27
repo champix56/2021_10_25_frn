a665582501a271f396dd68940ed2864f7e9ec3cf
alex <desorbaix@free.fr>
2021-10-27 16:33:18 +0200

    main with nav

--- src/App/components/MainActivity/MainActivity.tsx
+++ src/App/components/MainActivity/MainActivity.tsx
@@ -1,27 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {connect} from 'react-redux';
+import ProductEditor from '../ProductEditor/ProductEditor';
+import ProductListWithSearch from '../ProductListWithSearch/ProductListWithSearch';
 import style from './MainActivity.style';
-interface Props {}
 
-const initialState = {};
+interface IProps {}
+
+interface DispatchProps {
+  gotoList: Function;
+  gotoEditor: Function;
+}
+//creation de l'interface composée des props + types des maps
+type Props = DispatchProps & IProps;
+
 const MainActivity: React.FunctionComponent<Props> = props => {
-  const [state, setstate] = useState(initialState);
-  useEffect(() => {
-    return () => {};
-  }, []);
   return (
     <View style={style.container}>
       <Text style={style.title}>Ma super App</Text>
       <View style={style.buttons}>
-        <TouchableOpacity style={style.button}>
-          <View >
+        <TouchableOpacity
+          style={style.button}
+          onPress={() => {
+            props.gotoList();
+          }}>
+          <View>
             <Image source={require('../../img/list.png')} style={style.image} />
             <Text style={style.textButton}>List Produit</Text>
           </View>
         </TouchableOpacity>
 
         <View style={style.button}>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              props.gotoEditor();
+            }}>
             <Image
               source={require('../../img/editor.png')}
               style={style.image}
@@ -33,5 +46,20 @@ const MainActivity: React.FunctionComponent<Props> = props => {
     </View>
   );
 };
-
-export default MainActivity;
+const mapStateToProps = (state, own) => {
+  return own;
+};
+const mapDispatchToProps = (dispatch: Function) => {
+  return {
+    gotoList: () =>
+      dispatch({type: 'SET_WINDOW', value: <ProductListWithSearch />}),
+    gotoEditor: () =>
+      dispatch({
+        type: 'SET_WINDOW',
+        value: (
+          <ProductEditor produit={{name: '', img: '', prix: 0, desc: ''}} />
+        ),
+      }),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(MainActivity);
