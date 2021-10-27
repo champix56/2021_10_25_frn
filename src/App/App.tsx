eccd68e1fc8dc9992bfd9f7a688d752cf645f89b
alex <desorbaix@free.fr>
2021-10-27 12:55:17 +0200

    passage par redux avec provider

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,52 +8,28 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, ScrollView, Text} from 'react-native';
-import {IProduct} from '../interfaces/IProduct';
+import React from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
+import {Provider} from 'react-redux';
 import ProductSearch from './components/ProductSearch/ProductSearch';
 import ProductsList from './components/ProductsList/ProductsList';
-import {ADR_REST, RESSOURCES_NAME} from './config/config';
-const initialState: Array<IProduct> = [];
+import {store} from './store/store';
+
 const App = () => {
-  // const [counter, setcounter] = useState(0);
-  const [products, setproducts] = useState(initialState);
-  const [filtredProduct, setfiltredProduct] = useState(initialState);
-  const [search, setsearch] = useState('');
-  useEffect(() => {
-    console.log('Dans APP la search = ' + search);
-    if (search.length === 0) {
-      setfiltredProduct(products);
-    } else {
-      setfiltredProduct(
-        products.filter(e => {
-          return e.name
-            .toLocaleLowerCase()
-            .includes(search.toLocaleLowerCase());
-        }),
-      );
-    }
-  }, [search, products]);
-  useEffect(() => {
-    fetch(`${ADR_REST}${RESSOURCES_NAME.products}`)
-      .then(f => f.json())
-      .then(a => setproducts(a));
-  }, []);
+  // useEffect(() => {
+  //   store.subscribe(() => {
+  //     console.log(store.getState());
+  //   });
+  // }, []);
   return (
-    <SafeAreaView>
-      {/* <StatusBar /> */}
-      <ProductSearch
-        value={search}
-        onChange={(text: string) => {
-          setsearch(text);
-        }}
-      />
-      <Text>{search}</Text>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <ProductsList products={filtredProduct} />
-      </ScrollView>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView>
+        <ProductSearch />
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <ProductsList />
+        </ScrollView>
+      </SafeAreaView>
+    </Provider>
   );
 };
-
 export default App;
