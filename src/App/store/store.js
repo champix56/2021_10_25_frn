d22806c7acfad08533d901065fb91c309b98ecfa
alex <desorbaix@free.fr>
2021-10-27 17:30:23 +0200

    store complet

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -6,11 +6,18 @@ const initialState = {
   products: [],
   filtredProducts: [],
   search: '',
+  selectedProduct: {
+    name: '',
+    desc: '',
+    img: '',
+    prix: 0,
+  },
 };
 export const PRODUCTS_ACTIONS = Object.freeze({
   ADD_PRODUCTS: 'ADD_PRODUCTS',
   SET_SEARCH: 'SET_SEARCH',
   INIT: 'INIT',
+  SAVE_CURRENT: 'SAVE_CURRENT',
 });
 let interv = undefined;
 const reducerProduit = (state = initialState, action) => {
@@ -19,6 +26,46 @@ const reducerProduit = (state = initialState, action) => {
     ? 'START_PULLING'
     : action.type;
   switch (type) {
+    case 'GO_HOME':
+      return {...state, selectedProduct: initialState.selectedProduct};
+    case PRODUCTS_ACTIONS.SAVE_CURRENT:
+      fetch(
+        `${ADR_REST}${RESSOURCES_NAME.products}${
+          undefined !== state.selectedProduct.id
+            ? '/' + state.selectedProduct.id
+            : ''
+        }`,
+        {
+          method: `${undefined !== state.selectedProduct.id ? 'PUT' : 'POST'}`,
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(state.selectedProduct),
+        },
+      )
+        .then(f => f.json())
+        .then(o => {
+          // store.dispatch({type: 'UPDATE_PRODUCT', value: o});
+          store.dispatch({type: PRODUCTS_ACTIONS.INIT});
+        });
+      return state;
+    case 'UPDATE_PRODUCT':
+      const position = state.findIndex(e => e.id === action.value.id);
+      let produits = [];
+      if (position >= 0) {
+        let produits = [
+          ...state.products.slice(0, position),
+          action.value,
+          ...state.products.slice(position + 1),
+        ];
+        return reducerProduit(
+          {...state, products: produits},
+          {type: PRODUCTS_ACTIONS.SET_SEARCH, value: state.search},
+        );
+      } else {
+        return reducerProduit(
+          {...state, products: [...state.products, action.value]},
+          {type: PRODUCTS_ACTIONS.SET_SEARCH, value: state.search},
+        );
+      }
     case PRODUCTS_ACTIONS.INIT:
       fetch(`${ADR_REST}${RESSOURCES_NAME.products}`)
         .then(f => f.json())
@@ -37,11 +84,7 @@ const reducerProduit = (state = initialState, action) => {
       interv = interv ? clearInterval(interv) : null;
       return state;
     case PRODUCTS_ACTIONS.ADD_PRODUCTS:
-      return {
-        ...state,
-        products: action.values,
-        filtredProducts: action.values,
-      };
+      return reducerProduit({...state,products: action.values},{type: PRODUCTS_ACTIONS.SET_SEARCH, value: state.search});
     case PRODUCTS_ACTIONS.SET_SEARCH:
       return {
         ...state,
